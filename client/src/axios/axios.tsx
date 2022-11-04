95a472dcd44413fd5a06bdeb870dbe47e9d2cd5c
B-jisoo <mrk334kr@naver.com>
2022-11-04 13:45:47 +0900

    axios 경로 설정

--- client/src/axios/axios.tsx
+++ client/src/axios/axios.tsx
@@ -4,5 +4,5 @@ import axios from "axios";
  *  baseURL이 http://localhost:4000인 aixos
  */
 export default axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://martket-j.herokuapp.com/",
 });
